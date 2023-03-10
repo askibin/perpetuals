06d94dde9555c8abb428c2115cefe16fd9b5ef90
kristohberg <kristoffer.hovland.berg@gmail.com>
2023-03-10 12:47:11 +0100

    Add vercel router

--- dapp/src/routes/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import type { LayoutServerLoad } from './$types';
-
-export const load = (({ locals }) => {
-	// if (!locals.user) {
-	// 	throw redirect(307, '/long');
-	// }
-}) satisfies LayoutServerLoad;
--- dapp/svelte.config.js
+++ dapp/svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
